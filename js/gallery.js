af9b1e47a9d63d83ecca9dba597f202e38022e51
lowengagement <andychen97@gmail.com>
2025-09-09 01:21:26 -0400

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -3,12 +3,22 @@ l.className = 'lightbox';
 l.innerHTML = '<img>';
 document.body.appendChild(l);
 
-const i = l.firstElementChild;
+document.querySelectorAll('.gallery img').forEach(img => {
+    const t = new Image();
+    t.onload = () => {
+        const c = document.createElement('canvas');
+        c.width = img.offsetWidth || 300;
+        c.height = (t.height * c.width) / t.width;
+        c.getContext('2d').drawImage(t, 0, 0, c.width, c.height);
+        img.dataset.full = img.src;
+        img.src = c.toDataURL('image/jpeg', 0.7);
+    };
+    t.src = img.src;
+});
 
 document.onclick = e => {
     if (e.target.matches('.gallery img')) {
-        i.src = e.target.src;
-        i.alt = e.target.alt;
+        l.firstElementChild.src = e.target.dataset.full;
         l.style.display = 'block';
     } else if (e.target === l) {
         l.style.display = 'none';
